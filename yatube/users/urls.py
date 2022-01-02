2b9eaddfa540cffe58da481250b638e949a4a75c
Denis Kudakov <denn88kudakov@yandex.ru>
2022-01-02 20:02:01 +0300

    Финальный проект спринта выполнен и исправлен

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+import django.contrib.auth.views as dc
 from django.urls import path
 from . import views
 
@@ -9,12 +9,48 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
         'logout/',
-        LogoutView.as_view(template_name='users/logged_out.html'),
+        dc.LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
     ),
     path(
         'login/',
-        LoginView.as_view(template_name='users/login.html'),
+        dc.LoginView.as_view(template_name='users/login.html'),
         name='login'
+    ),
+    path(
+        'password_reset_form/',
+        dc.PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'),
+        name='password_reset_form'
+    ),
+    path(
+        'password_reset_confirm/',
+        dc.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password_reset_done/',
+        dc.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'),
+        name='password_reset_done'
+    ),
+    path(
+        'password_reset_confirm/',
+        dc.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password_change_form/',
+        dc.PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'),
+        name='password_change_form'
+    ),
+    path(
+        'password_change_done/',
+        dc.PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'),
+        name='password_change_done'
     ),
 ]
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -86,9 +86,9 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 LOGOUT_REDIRECT_URL = 'posts:index'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
