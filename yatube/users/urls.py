d82aecec658ee659620db2cce783a210982741ae
Denis Kudakov <denn88kudakov@yandex.ru>
2022-01-03 17:41:19 +0300

    Добавлены страницы регистраци, авторизации и смены пароля

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,5 +1,5 @@
 import django.contrib.auth.views as dc
-from django.urls import path
+from django.urls import path ,reverse_lazy
 from . import views
 
 
@@ -18,37 +18,38 @@ urlpatterns = [
         name='login'
     ),
     path(
-        'password_reset_form/',
+        'password_reset/',
         dc.PasswordResetView.as_view(
             template_name='users/password_reset_form.html'),
         name='password_reset_form'
     ),
     path(
-        'password_reset_confirm/',
+        'auth/reset/<uidb64>/<token>/',
         dc.PasswordResetConfirmView.as_view(
-            template_name='users/password_reset_confirm.html'),
+            template_name='users/password_reset_confirm.html',
+            success_url=reverse_lazy('users:password_reset_complete')),
         name='password_reset_confirm'
     ),
     path(
-        'password_reset_done/',
+        'password_reset/done/',
         dc.PasswordResetDoneView.as_view(
             template_name='users/password_reset_done.html'),
         name='password_reset_done'
     ),
     path(
-        'password_reset_confirm/',
-        dc.PasswordResetConfirmView.as_view(
-            template_name='users/password_reset_confirm.html'),
-        name='password_reset_confirm'
+        'reset/done/',
+        dc.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'),
+        name='password_reset_complete'
     ),
     path(
-        'password_change_form/',
+        'password_change/',
         dc.PasswordChangeView.as_view(
             template_name='users/password_change_form.html'),
         name='password_change_form'
     ),
     path(
-        'password_change_done/',
+        'password_change/done/',
         dc.PasswordChangeDoneView.as_view(
             template_name='users/password_change_done.html'),
         name='password_change_done'
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -87,8 +87,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_URL = 'users:login'
-LOGIN_REDIRECT_URL = 'posts:index'
-LOGOUT_REDIRECT_URL = 'posts:index'
+LOGIN_REDIRECT_URL = 'posts:main_page'
+#LOGOUT_REDIRECT_URL = 'users:logout'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
